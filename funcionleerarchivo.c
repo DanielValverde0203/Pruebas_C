27e15c6cbe65f8a80978f09137b390dec2cbedc2
DanielValverde0203 <143844258+DanielValverde0203@users.noreply.github.com>
2023-10-15 18:12:00 -0600

    Add files via upload

---
+++ funcionleerarchivo.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	FILE *image_in;
+	char name[50];
+	char *info = NULL;
+	lonf size;
+
+	scanf("%s", image_in);
+
+	imagein = fopen(image_in, "rb");
+}
+
