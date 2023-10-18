43c7ca9f33eee155f3ea4970dada507ef4336714
PSalasG <pablo.salas@ucr.ac.cr>
2023-10-17 19:19:04 -0600

    a

--- argumentos_proy2.c
+++ argumentos_proy2.c
@@ -16,12 +16,12 @@ void ayuda () {
         printf("\n");
 	printf("Algoritmos (van después de indicar el nombre del archivo de entrada y el de salida):\n");
 	printf("\n");
-	printf("rotate  recibe angulo de rotación de la imagen de entrada, la imagen resultante es la misma pero girada ese ángulo\n");
-        printf("otro    coming soon\n");
+	printf("rotate  gira la imagen 90 grados\n");
+        printf("grayscl	convierte la imagen a la escala de grises\n");
 	printf("\n");
        	printf("Ejemplos:\n");
         printf("\n");
-	printf("modif_image.exe -i imagen_entrada -o imagen_salida rotate 90\n");
+	printf("modif_image.exe -i imagen_entrada -o imagen_salida rotate\n");
 	printf("Nota: se debe respetar el órden de argumentos presente en el ejemplo anterior.\n");
 }
 
@@ -43,7 +43,11 @@ int main (int argc, char *argv[]) {
 		printf("Ingrese más argumentos\n");
 		return 1;
 	}
-	
+
+	FILE *image_in;
+	FILE *image_out;
+	char *formato;
+
 	for (int i=1; i < argc; i++) {
 
 		// Este argumento se debe poner solo ya que solamente imprime el mensaje de ayuda y termina el programa.
@@ -55,21 +59,51 @@ int main (int argc, char *argv[]) {
 		// Verifica que se incluya el argumento -i.
 		else if (strcmp(argv[i], "-i") == 0) {
 			
-			printf("Se guarda el tercer argumento como el nombre de la imagen.\n");
 			printf("Path img original: %s\n", argv[2]);
 			
-			char *formato = checkear(argv[2]);
+			image_in = fopen(argv[2],"r");
+
+			formato = checkear(argv[2]);
+
 			printf("Formato: %s\n", formato);
 
 		}
 
 		// Verifica que se incluya el argumento -o.
 		else if (strcmp(argv[i], "-o") == 0) {
-
-			printf("Se guarda el quinto argumento en una variable para usarlo como nombre del archivo resultante.\n");
+			
+			image_out = fopen(argv[4], "w+");
+			
 			printf("Path img resultante: %s\n", argv[4]);
 		}
 	}
+	
+	if (strcmp(argv[5], "rotate") == 0) {
+	
+		if (strcmp(formato, "jpeg") == 0){
+                        //llamar_funcion_rotate_jpeg()
+			printf("Rotar jpeg\n");
+                }
+                else if (strcmp(formato, "png") == 0){
+                        //llamar_funcion_rotate_png()
+                	printf("Rotar png\n");
+		}
+	}
+
+	else if (strcmp(argv[5], "gray_scale") == 0) {
+	
+		if (strcmp(formato, "jpeg") == 0){
+                        //llamar_funcion_gray_scale_jpeg()
+                	printf("Gris jpeg\n");
+		}
+                else if (strcmp(formato, "png") == 0){
+                        //llamar_funcion_gray_scale_png()
+                	printf("Gris png\n");
+		}
+	}
+
+	fclose(image_in);
+	fclose(image_out);
 
 	return 0;
 }
